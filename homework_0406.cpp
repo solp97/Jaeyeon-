38d82ae9e4b1363ece167095e8e8bc9412cacd61
solp97 <103083705+solp97@users.noreply.github.com>
2022-04-07 09:41:24 +0900

    Add files via upload

---
+++ homework_0406.cpp
@@ -0,0 +1,76 @@
+﻿// homework_0406.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string myName = "최재연\n";
+    cout << "내 이름:" << myName << endl;
+    
+    int myTall = 185;
+    cout << "내 키:" << myTall << endl;
+
+    
+    string myBloodType = "O";
+    cout << "내 혈액형:" << myBloodType << endl;
+
+    string myLike = "게임, 영화, 자전거, 팝송";
+    cout << "좋아하는것: " << myLike << endl;
+
+    string myDisLike = "냄새심한 음식";
+    cout << "싫어하는것: " << myDisLike << endl;
+
+    string myHobby = "게임, 음악듣기";
+    cout << "취미: " << myHobby << endl;
+
+    string myLoveCar = "재규어";
+    cout << "좋아하는 자동차: " << myLoveCar << endl;
+
+    string myLoveGame = "스팀 게임, 모든장르";
+    cout << "좋아하는 게임: " << myLoveGame << endl;
+
+    string myLove = "순수함";
+    cout << "이상형: " << myLove << endl;
+
+    string myJob = "백수";
+    cout << "하는 일: " << myJob << endl;
+
+    string myLovePOP = "게임, 음악듣기";
+    cout << "좋아하는 팝송 " << myLovePOP << endl;
+
+    string myDislikeMusic = "트로트";
+    cout << "싫어하는 노래 " << myDislikeMusic << endl;
+
+    string myDislikeGame = "없음";
+    cout << "싫어하는 게임 " << myDislikeGame << endl;
+
+    string myDislove = "게으름, 나대기";
+    cout << "싫어하는 이상형 " << myDislove << endl;
+
+    string myDream = "유명한 게임개발자";
+    cout << " 꿈: " << myDream << endl;
+
+    int myDreamAge = 110;
+    cout << " 원하는 수명: " << myDreamAge << endl;
+
+    string myFirstDream = "스카이다이빙";
+    cout << " 가장 하고싶은 것: " << myFirstDream << endl;
+
+    string DreamGame = "유저들에게 웃음을 주는 게임";
+    cout << " 원하는 게임: " << DreamGame << endl;
+
+    string LoveDog = "골든리트리버";
+    cout << " 좋아하는 개: " << LoveDog << endl;
+
+    string disLoveDog = "치와와, 불독";
+
+    cout << " 싫어하는 개: " << disLoveDog << endl;
+
+
+    
+}
+
